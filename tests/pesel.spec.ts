780999d466b31586a830e6cfb3cf4945a22db8a7
bmanczak <bmanczak@fandom.com>
2021-11-25 18:47:55 +0100

    dummy title change

--- tests/pesel.spec.ts
+++ tests/pesel.spec.ts
@@ -5,7 +5,7 @@ import { PeselPage } from './pesel.page';
 test('should display welcome message', async ({ page }) => {
     const peselPage = new PeselPage(page);
     await peselPage.goto();
-    expect(await peselPage.getTitleText()).toBe('Walidator PESEL');
+    expect(await peselPage.getTitleText()).toBe('Walidatorek PESEL');
 });
 
 test('should accept valid PESEL', async ({ page }) => {
